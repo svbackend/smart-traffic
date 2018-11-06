98a5af51b3c0df622322a1eda9cb7a1e1b319ed3
Valentyn Saik <svbackend22@gmail.com>
2018-11-06 12:12:19 +0100

    Map markup

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="map">
+      <div class="line-y" v-for="y in verticalLines">
+        <div class="line-x" v-for="x in horizontalBoxes">
+          <div class="box">{{ y }} / {{ x }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+    data() {
+      return {
+          positions: [],
+          verticalLines: [],
+          horizontalBoxes: [],
+          y: [],
+          x: [],
+      }
+    },
+    methods: {
+      generateMap() {
+
+          for (let y = 0; y++< 20;) {
+              this.verticalLines.push(y)
+            for (let x = 0; x++ < 10;) {
+              let position = {x: x, y: y};
+              this.positions.push(position)
+            }
+          }
+
+          for (let x = 0; x++ <= 20;) {
+              this.horizontalBoxes.push(x)
+          }
+      }
+    },
+    created() {
+      this.generateMap();
+    }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +53,27 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+  .map {
+    margin: 0 auto;
+    max-width: 100%;
+    .line-x {
+      display: inline;
+    }
+    .line-y {
+      width: 100%;
+      max-width: 100%;
+      overflow: hidden;
+    }
+    .box {
+      display: inline-block;
+      width: 36px;
+      height: 36px;
+      vertical-align: middle;
+      padding: 4px;
+      background-color: black;
+      &.newLine {
+
+      }
+    }
+  }
 </style>
