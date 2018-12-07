19ed4792f611231822acd39430f1f3518486a2ec
Valentyn Saik <svbackend22@gmail.com>
2018-12-07 10:11:56 +0100

    Refactoring: move map to separate file (not ready yet)

---
+++ src/maps/simpleCrossingMap.js
@@ -0,0 +1,11 @@
+/**
+ * y: [0,1,2,3,4...],
+ * x: [0,1,2,3,4...],
+ * road: [{x: 0,y:0},{x: 1,y:1}...],
+ * lights: [{position: {x: 0, y:0}, color: "green/red", },],
+ */
+export function getMap() {
+  for (let i = 0; i) {
+
+  }
+}
