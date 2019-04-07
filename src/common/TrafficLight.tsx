5076e1b4dea588b052f65ee9da2a3628000112e6
Valentyn Saik <svbackend22@gmail.com>
2019-04-07 17:36:22 +0100

    Remove IPosition

--- src/common/TrafficLight.tsx
+++ src/common/TrafficLight.tsx
@@ -1,13 +1,15 @@
-import { TrafficLight as TrafficLightInterface, Position as PositionInterface } from "../interfaces";
+import { TrafficLight as TrafficLightInterface } from "../interfaces";
 import { TrafficLightState } from "./TrafficLightState";
+import { Position } from "./Position";
 
 export class TrafficLight implements TrafficLightInterface {
-    watchedPositions: Array<PositionInterface>;
+    watchedPositions: Array<Position>;
     conflicts: Array<TrafficLightInterface>;
     state: TrafficLightState;
-    stopPositions: Array<PositionInterface>;
+    stopPositions: Array<Position>;
+    amountOfTraffic: number = 0;
 
-    constructor(watchedPositions: Array<PositionInterface>, conflicts: Array<TrafficLightInterface>, stopPositions: Array<PositionInterface>) {
+    constructor(watchedPositions: Array<Position>, conflicts: Array<TrafficLightInterface>, stopPositions: Array<Position>) {
         this.watchedPositions = watchedPositions;
         this.stopPositions = stopPositions;
         this.conflicts = conflicts;
